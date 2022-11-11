832962a13371e9321ab2124e553eefa83c0152ac
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2022-11-11 15:00:19 +0100

    models

---
+++ Planner.core/Models/Tag.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Planner.core.Models;
+
+public class Tag
+{
+    [Key]
+    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string? Name { get; set; }
+    public string? ColorHex { get; set; }
+}
--- Planner.core/Models/ToDo.cs
+++ Planner.core/Models/ToDo.cs
@@ -1,22 +1,21 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace Planner.core.Models
+namespace Planner.core.Models;
+
+public class ToDo
 {
-    public class ToDo
-    {
-        [Key]
-        public required string Id { get; set; } = Guid.NewGuid().ToString();
-        public required string Name { get; set; }
-        public required string Description { get; set; }
-        public DateTimeOffset StartDate { get; set; } = DateTimeOffset.Now;
-        public DateTimeOffset? EndDate { get; set;}
+    [Key]
+    public required string Id { get; set; } = Guid.NewGuid().ToString();
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+    public DateTimeOffset StartDate { get; set; } = DateTimeOffset.Now;
+    public DateTimeOffset? EndDate { get; set;}
 
-        /// <summary>
-        /// serialized list of this todo tasks
-        /// </summary>
-        public string? ToDoList { get; set; }
-        public string? CollaboratorId { get; set; }
+    /// <summary>
+    /// serialized list of this todo tasks
+    /// </summary>
+    public string? ToDoList { get; set; }
+    public string? CollaboratorId { get; set; }
 
-        public virtual Collaborator? Collaborator { get; set; }
-    }
+    public virtual Collaborator? Collaborator { get; set; }
 }
